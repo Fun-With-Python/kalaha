2a32788c4e72825cb336cec2bcfa5bbb5eca0b2e
Jakob <joghurt_jakob@live.de>
2012-05-09 10:26:46 +0200

    recreatet gamelogic

--- gamelogic.py
+++ gamelogic.py
@@ -1,5 +1,6 @@
 import outputgametable as out
-
-		
-out.score[out.next["U0"]] = 5
+player1 = "Player 1"
+player2 = "Player 2"
 out.output()
+while 1:
+	inp = input(player1 + ", please insert a number between 1-6: ")
