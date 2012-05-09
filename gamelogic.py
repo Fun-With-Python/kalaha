a4a6dc9c63e4e33bddd01e5d1a0b54d6ea076fd0
Nanak Tattyrek <nanak@kabsi.at>
2012-05-09 10:39:26 +0200

    added exceptionhandler

--- gamelogic.py
+++ gamelogic.py
@@ -1,6 +1,22 @@
 import outputgametable as out
+import sys
+
 player1 = "Player 1"
 player2 = "Player 2"
 out.output()
 while 1:
-	inp = input(player1 + ", please insert a number between 1-6: ")
+	try:
+		inp = int(input(player1 + ", please insert a number between 1-6: "))
+	except ValueError:
+		print("Couldn\'t convert to Integer")
+		continue
+	except KeyboardInterrupt:
+		print("\n  ^C detected, terminating...")
+		sys.exit()
+	except EOFError:
+		print("\n  ^D detected, terminating...")
+		sys.exit()
+	
+	if inp < 1 or inp > 6:
+		print ("Out of range (1 - 6)")
+		continue
