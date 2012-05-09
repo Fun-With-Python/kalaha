5997e9081aa8cb2ca24a8c37e8b5feb55d79095f
Jakob <joghurt_jakob@live.de>
2012-05-09 10:09:06 +0200

    implemented next as dict

--- gamelogic.py
+++ gamelogic.py
@@ -1,10 +1,5 @@
 import outputgametable as out
-def next(now):
-	flag = 0
-	for k, v in out.score.items():
-		if flag:
-			return k
-		flag = k == now
 
-out.score["U0"] = 5
+		
+out.score[out.next["U0"]] = 5
 out.output()
