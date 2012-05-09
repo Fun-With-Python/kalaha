45edc8a7498dc7796ccd05b0d53ca9700f93f5c3
Jakob <joghurt_jakob@live.de>
2012-05-09 12:03:18 +0200

    extended exception handling in gamelogic

--- gamelogic.py
+++ gamelogic.py
@@ -1,15 +1,11 @@
 import outputgametable as out
 import sys
-
-player1 = "Player 1"
-player2 = "Player 2"
-out.output()
-while 1:
+def safeinputint(text):
 	try:
-		inp = int(input(player1 + ", please insert a number between 1-6: "))
+		inp = int(input(text))
 	except ValueError:
 		print("Couldn\'t convert to Integer")
-		continue
+		return safeinputint(text)
 	except KeyboardInterrupt:
 		print("\n  ^C detected, terminating...")
 		sys.exit()
@@ -19,4 +15,15 @@ while 1:
 	
 	if inp < 1 or inp > 6:
 		print ("Out of range (1 - 6)")
-		continue
+		return safeinputint(text)
+	else:
+		return inp
+
+player1 = "Player 1"
+player2 = "Player 2"
+while 1:
+	print("*")
+	out.output()
+	print("\033[31;42m*\033[m")
+	inp = safeinputint(player1 + ", please insert a number between 1-6: ")
+	
